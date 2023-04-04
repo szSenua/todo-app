a92646b82921c23931a80e9a5031a871ff7ee7ba
Senua <chc0089@gmail.com>
2023-04-05 00:32:43 +0200

    Creating our first store

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,14 @@
+<script setup>
+import TodoForm from './TodoForm.vue'
+import TodoList from './TodoList.vue'
+</script>
+
+<template>
+    <div class="todo-app"></div>
+    <h1>To Do List</h1>
+    <todo-form />
+    <todo-list />
+</template>
+
+<style scoped>
+</style>
---
+++ src/stores/todoList.js
@@ -0,0 +1,11 @@
+import { defineStore } from "pinia";
+
+export const useTodoListStore = defineStore( 'todoList', {
+    //state
+    //actions
+    //getters
+    state: () => {
+        todoList: []
+    }
+
+})
